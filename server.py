f6ee8f69d6a77897c1f838a10c34641c7acf35ab
Amr Mostafa <amrmos.it@gmail.com>
2025-04-26 21:00:20 +1000

    Adding initial structure for the RESTful server

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
 # Creating small RESTful API
+
+# Import the Flask class from the flask module, & jsonify
+from flask import Flask, jsonify
+
+# Create an instance of Flask class, passing the name of the current module
+app = Flask(__name__)
+
+# Define a route for the root URL ('/')
+@app.route('/')
+def index():
+    # function thaat handles requests to the root URL
+    # return JSON response
+    return jsonify({"hello World!"})
